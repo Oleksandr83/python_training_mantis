88d0d4a65d92e1b01753b13cc40e663e5a45cb16
Oleksandr83 <kamika@bigmir.net>
2020-10-13 13:04:43 +0200

    добавлена генерация создаваемого проекта

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,10 +1,20 @@
 from model.project import Project
+import pytest
+import string
+import random
 
-def test_add_project(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5 #+ string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata =[Project(project_name=random_string("name", 10), project_description=random_string("description", 25))]
+
+
+@pytest.mark.parametrize("project", testdata, ids=[repr(x) for x in testdata])
+def test_add_project(app, project):
     old_project_list = app.project.get_projacts_list()
     print(old_project_list)
-    #app.session.login("administrator", "root")
-    project = Project(project_name="New 3", project_description="kjbk")
     app.project.create_project(project)
     new_project_list = app.project.get_projacts_list()
     old_project_list.append(project)
